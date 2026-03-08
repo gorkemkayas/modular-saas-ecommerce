453eb9dc5ca970ca096e01eb921402b71145fae8
Görkem Kaya <gorkemkayas@hotmail.com>
2026-03-08 14:06:54 +0300

    Add DbContext and EF Configuration of 'Store'.

---
+++ src/Modules/Store/Store.Infrastructure/Persistance/Configurations/StoreConfiguration.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Store.Domain.ValueObjects;
+
+namespace Store.Infrastructure.Persistance.Configurations
+{
+    public sealed class StoreConfiguration : IEntityTypeConfiguration<Store.Domain.Stores.Store>
+    {
+        public void Configure(EntityTypeBuilder<Store.Domain.Stores.Store> builder)
+        {
+            builder.ToTable("Stores");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.TenantId)
+                .IsRequired();
+
+            builder.Property(x => x.Name)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(x => x.Slug)
+                .HasConversion(
+                    slug => slug.Value,
+                    value => Slug.Create(value))
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(x => x.Description)
+                .HasMaxLength(1000);
+
+            builder.Property(x => x.LogoUrl)
+                .HasMaxLength(500);
+
+            builder.Property(x => x.Status)
+                .IsRequired();
+
+            builder.Property(x => x.IsPublished)
+                .IsRequired();
+
+            builder.Property(x => x.CreatedAtUtc)
+                .IsRequired();
+
+            builder.Property(x => x.UpdatedAtUtc);
+
+            builder.HasIndex(x => x.TenantId)
+                .IsUnique();
+
+            builder.HasIndex(x => x.Slug)
+                .IsUnique();
+        }
+    }
+}
---
+++ src/Modules/Store/Store.Infrastructure/Persistance/StoreDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Store.Application.Abstractions;
+
+namespace Store.Infrastructure.Persistance
+{
+    public sealed class StoreDbContext : DbContext, IUnitOfWork
+    {
+        public StoreDbContext(DbContextOptions<StoreDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Store.Domain.Stores.Store> Stores => Set<Store.Domain.Stores.Store>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(StoreDbContext).Assembly);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
