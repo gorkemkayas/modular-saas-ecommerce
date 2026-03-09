ecd7eec1c37915674a7d8d4044ca34d90ff43775
Görkem Kaya <gorkemkayas@hotmail.com>
2026-03-09 10:31:50 +0300

    Add UnpublishStore use case.

---
+++ src/Modules/Store/Store.Application/Stores/Commands/UnpublishStore/UnpublishStoreCommand.cs
@@ -0,0 +1,4 @@
+﻿namespace Store.Application.Stores.Commands.UnpublishStore
+{
+    public sealed record UnpublishStoreCommand(Guid TenantId);
+}
---
+++ src/Modules/Store/Store.Application/Stores/Commands/UnpublishStore/UnpublishStoreCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Store.Application.Abstractions;
+using Store.Application.Stores.Commands.PublishStore;
+using Store.Domain.Stores;
+
+namespace Store.Application.Stores.Commands.UnpublishStore
+{
+    public sealed class UnpublishStoreCommandHandler
+    {
+        private readonly IStoreRepository _repository;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UnpublishStoreCommandHandler(IStoreRepository repository, IUnitOfWork unitOfWork)
+        {
+            _repository = repository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task Handle(PublishStoreCommand command, CancellationToken cancellationToken)
+        {
+            var store = await _repository.GetByTenantIdAsync(command.TenantId, cancellationToken);
+            if (store == null)
+                throw new InvalidOperationException("Store not found.");
+
+            store.Unpublish();
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
